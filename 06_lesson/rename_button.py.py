f45bf5bc7c920539d68ff66bc4d64a6ac552bece
Marina20061983 <prohorowa.marina@ya.ru>
2026-04-02 20:26:21 +0300

    six commit

---
+++ 06_lesson/rename_button.py.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# Устанавливаем драйвер для Chrome
+browser = webdriver.Chrome(
+    service=ChromeService(
+        ChromeDriverManager().install()
+    )
+)
+
+try:
+    # Переходим на указанный сайт
+    browser.get("http://uitestingplayground.com/textinput")
+
+    # Находим поле ввода и вводим текст "SkyPro"
+    text_input = browser.find_element(By.ID, "newButtonName")
+    text_input.send_keys("SkyPro")
+
+    # Находим синюю кнопку и нажимаем на нее
+    button = browser.find_element(By.ID, "updatingButton")
+    button.click()
+
+    # Получаем текст кнопки и выводим его в консоль
+    button_text = button.text
+    print(button_text)
+
+finally:
+    # Закрываем браузер
+    sleep(3)  # ждем немного, чтобы увидеть результат
+    browser.quit()
